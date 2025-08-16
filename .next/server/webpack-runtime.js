3f728d9b61b2b213faa8e6a619d7f3efb940468b
salommmmmsabd <vvor2090@gmail.com>
2025-08-16 13:53:56 +0300

    Updated app-paths-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("27b5642e55cd6a40")
+/******/ 		__webpack_require__.h = () => ("8026965152afbc15")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
